d5133e73e0c5d67633c928091bee47ad3abf3950
chasing-a-dream <s12k12@mail.ru>
2024-01-12 23:13:13 +0300

    Edit print and insert function

--- houseInfo/family/family.go
+++ houseInfo/family/family.go
@@ -18,31 +18,47 @@ func CreateFamily() []Family {
 	familyMembers := []Family{
 		{Name: "Elena", Age: 45, Status: "Mother", Room: "bedroom1"},
 		{Name: "Ksenia", Age: 20, Status: "Daughter", Room: "bedroom2"},
+		{Name: "Sergei", Age: 21, Status: "Step-son", Room: "living_room"},
+		{Name: "TAGIR", Age: 98, Status: "Brother", Room: "bathroom"},
+		{Name: "Danechka", Age: 20, Status: "family_friend", Room: "living_room"},
 	}
 	return familyMembers
 }
 
-func InsertInFamily(member Family, conn pgx.Conn) {
-	bd, err := conn.Begin(context.Background())
-	if err != nil {
-		fmt.Println(err)
+func InsertInFamily(members []Family, conn pgx.Conn) {
+	for index := 0; index < len(members); index++ {
+		bd, beginErr := conn.Begin(context.Background())
+		if beginErr != nil {
+			fmt.Println(beginErr)
+			return
+		}
+		defer bd.Rollback(context.Background())
+		_, execErr := bd.Exec(context.Background(), "insert into family_members(name, age, status, room) values ($1, $2, $3, $4)",
+			members[index].Name, members[index].Age, members[index].Status, members[index].Room)
+		if execErr != nil {
+			fmt.Println(execErr)
+			return
+		}
+		err2 := bd.Commit(context.Background())
+		if err2 != nil {
+			fmt.Println(err2)
+			return
+		}
 	}
-	defer bd.Rollback(context.Background())
-	bd.Exec(context.Background(), "insert into family_members(name, age, status, room) values ($1, $2, $3, $4)", member.Name, member.Age, member.Status, member.Room)
-	bd.Commit(context.Background())
 }
 
 func ShowFamily(conn pgx.Conn) {
-	var name, id, age, room string
-	rows, err := conn.Query(context.Background(), "select id, name, age, room from family_members")
+	var name, id, age, room, status string
+	rows, err := conn.Query(context.Background(), "select id, name, age, room, status from family_members")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	} else {
-		print("         ID     NAME     AGE       ROOM\n       ______________________________________\n")
+		print("        ID      NAME     AGE       ROOM             STATUS\n" +
+			"       ____________________________________________________\n")
 		for rows.Next() {
-			rows.Scan(&id, &name, &age, &room)
-			fmt.Printf("%10s %10s %s %15s\n", id, name, age, room)
+			rows.Scan(&id, &name, &age, &room, &status)
+			fmt.Printf("%10s %10s %5s %15s %15s\n", id, name, age, room, status)
 		}
 	}
 }

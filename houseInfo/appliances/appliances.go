03f057cbdf3285a84ab3498143a8414487be179b
chasing-a-dream <s12k12@mail.ru>
2024-01-12 23:23:09 +0300

    edited print function

--- houseInfo/appliances/appliances.go
+++ houseInfo/appliances/appliances.go
@@ -1,5 +1,12 @@
 package appliances
 
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"os"
+)
+
 type Appliance struct {
 	Name  string
 	Brand string
@@ -15,4 +22,42 @@ func CreateAppliances() []Appliance {
 		{Name: "TV", Brand: "Sony", Power: 200},
 	}
 	return appliances
+}
+
+func InsertInAppliances(appliance []Appliance, conn pgx.Conn) {
+	for index := 0; index < len(appliance); index++ {
+		bd, beginErr := conn.Begin(context.Background())
+		if beginErr != nil {
+			fmt.Println(beginErr)
+			return
+		}
+		defer bd.Rollback(context.Background())
+		_, execErr := bd.Exec(context.Background(), "insert into appliances(name, brand, power) "+
+			"values ($1, $2, $3)", appliance[index].Name, appliance[index].Brand, appliance[index].Power)
+		if execErr != nil {
+			fmt.Println(execErr)
+			return
+		}
+		err2 := bd.Commit(context.Background())
+		if err2 != nil {
+			fmt.Println(err2)
+			return
+		}
+	}
+}
+
+func ShowAppliances(conn pgx.Conn) {
+	var name, brand, power string
+	rows, err := conn.Query(context.Background(), "select name, brand, power from appliances")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	} else {
+		print("\n                NAME            BRAND        POWER\n" +
+			"        ___________________________________________\n")
+		for rows.Next() {
+			rows.Scan(&name, &brand, &power)
+			fmt.Printf("%25s %12s %12s\n", name, brand, power)
+		}
+	}
 }

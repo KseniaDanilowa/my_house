6809ccb72f2ba1ad33d58f9be0dc50bc692507fe
chasing-a-dream <s12k12@mail.ru>
2024-01-12 23:20:22 +0300

    moved dir

--- PrintHouseFromDb/printInfo.go
+++ PrintHouseFromDb/printInfo.go
@@ -3,14 +3,15 @@ package PrintHouseFromDb
 import (
 	"context"
 	"my_house/connection"
+	"my_house/houseInfo/appliances"
 	"my_house/houseInfo/family"
 	"my_house/houseInfo/furniture"
 )
 
 func PrintHouse() {
 	ctx := context.Background()
-	conn := connection.Make_conn(ctx)
+	conn := connection.MakeConn(ctx)
 	family.ShowFamily(conn)
 	furniture.ShowFurniture(conn)
-
+	appliances.ShowAppliances(conn)
 }

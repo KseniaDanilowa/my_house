16cdc6af5795b3a8c195b5af2fdd8b777b38b0a3
chasing-a-dream <s12k12@mail.ru>
2024-01-13 01:00:08 +0300

    final joint insert

--- InsertHouseData/Insert.go
+++ InsertHouseData/Insert.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"my_house/connection"
 	"my_house/houseInfo/appliances"
+	"my_house/houseInfo/family"
+	"my_house/houseInfo/furniture"
+	"my_house/houseInfo/houseParams"
 )
 
 func InsertData() {
+
 	ctx := context.Background()
 	conn := connection.MakeConn(ctx)
-	appliances.InsertInAppliances(appliances.CreateAppliances(), conn)
-	//furniture.InsertInFurniture(furniture.CreateFurniture(), conn)
-	//family.InsertInFamily(family.CreateFamily()[1], conn)
+	appliances.InsertInAppliances(appliances.CreateAppliance(), conn)
+	houseParams.InsertInHouseParams(houseParams.CreateHouseParams(), conn)
+	furniture.InsertInFurniture(furniture.CreateFurniture(), conn)
+	family.InsertInFamily(family.CreateFamily(), conn)
 }
